a454e83fc009c195266ae3d29de9f546cdc87a21
Pavel Vavilin <iq23life@gmail.com>
2022-11-02 16:06:18 +0300

    animation and clock (threejs)

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ import './style.css'
 
 // Сцена
 const scene = new THREE.Scene()
+
 // Объект и его геометрия и материал
 const geometry = new THREE.BoxGeometry(1,1,1,1)
 const material = new THREE.MeshBasicMaterial({color: 'purple', wireframe:true})
@@ -28,38 +29,51 @@ renderer.setSize(size.width, size.height)
 const axesHelper = new THREE.AxesHelper(3)
 scene.add(axesHelper)
 
-
 // Передвижение куба
-mesh.position.set(-1, -0.8, 0.4)
+mesh.position.set(0,0,-0.4)
 
 // Масштабирование куба
-mesh.scale.set(0.5, 2, 0.7)
+mesh.scale.set(1, 1, 1)
 
-// Вращение куба
-mesh.rotation.x = Math.PI * 0.25;
-mesh.rotation.y = Math.PI * 0.25;
+// Время
+const clock = new THREE.Clock();
 
-// Позиция камеры
+const animateLoop = () => {
+
+    const elapsedTime = clock.getElapsedTime()
+    
+    mesh.position.x = Math.cos(elapsedTime);
+    mesh.position.y = Math.sin(elapsedTime);
+    camera.position.x = Math.cos(-elapsedTime);
+    camera.position.y = Math.sin(-elapsedTime);
+    camera.lookAt(mesh.position);
+
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(animateLoop)
+}
+
+animateLoop()
+
+// // Позиция камеры
 // camera.lookAt( new THREE.Vector3(0, 0, -1))
 // camera.lookAt( mesh.position)
 
-const group = new THREE.Group()
-group.scale.y = 0.7;
-group.rotation.x= 0.5
-scene.add(group)
+// // Создание группы объектов
+// const group = new THREE.Group()
+// group.scale.y = 0.7;
+// group.rotation.x= 0.5
+// scene.add(group)
 
-const cube1 = new THREE.Mesh(geometry, material)
-cube1.position.x= -1.2
-group.add(cube1)
-const cube2 = new THREE.Mesh(geometry, material)
-cube2.position.x= 0
-group.add(cube2)
-const cube3 = new THREE.Mesh(geometry, material)
-cube3.position.x= 1.2
-group.add(cube3)
-
-renderer.render(scene, camera)
-
+// const cube1 = new THREE.Mesh(geometry, material)
+// cube1.position.x= -1.2
+// group.add(cube1)
+// const cube2 = new THREE.Mesh(geometry, material)
+// cube2.position.x= 0
+// group.add(cube2)
+// const cube3 = new THREE.Mesh(geometry, material)
+// cube3.position.x= 1.2
+// group.add(cube3)
 
 // // длина вектора
 // console.log(mesh.position.length());
