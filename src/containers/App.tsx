6c9bad35aef483be73bbf2edc3754c02146882a5
Pavel Vavilin <iq23life@gmail.com>
2023-12-17 21:38:20 +0300

    resolved conflicts

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,14 +1,7 @@
-// import { useState } from 'react'
-import "./App.css";
-import { SvgType } from "@types/types";
+import './App.css';
 
 function App() {
-  // const [count, setCount] = useState(0)
-
-  return (
-    // {titleLogo}
-    <div>ky f</div>
-  );
+  return <div>ky f</div>;
 }
 
 export default App;
